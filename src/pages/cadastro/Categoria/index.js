20a0446003cf5af31739cfec2035b74719e57e1c
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-08-02 22:08:14 -0300

    Correcao de url pagina de cadastro de categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -22,7 +22,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:3333/categorias'
-      : 'https://devsoutinhoflix.herokuapp.com/categorias';
+      : 'https://servicebiflix.herokuapp.com/categorias';
     // E a ju ama variáveis
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
