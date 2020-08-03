3f9c4aceaad7469fba7be164dd9b0d9e675396bf
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-08-02 21:30:06 -0300

    troca de porta

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:3333'
+  ? 'http://localhost:8080'
   : 'https://devsoutinhoflix.herokuapp.com';
 
 export default {
